68484d06898340931c3ccbd9e615e2270cf85bbc
Dima_Os <osipovdima1989@gmail.com>
2021-02-07 22:59:51 +0200

    were added App

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+
+import s from './App.module.css';
+
+import ImageGallery from './components/ImageGallery';
+import ImageGalleryItem from './components/ImageGalleryItem';
+import Modal from './components/Modal';
+import Searchbar from './components/Searchbar';
+import imageFinderAPI from './services/imageFinderAPI';
+import PuffLoader from './components/Loader';
+import Button from './components/Button';
+
+export default class App extends Component {
+  state = {
+    userQuery: '',
+    images: [],
+    currentPage: 1,
+    error: null,
+    showModal: false,
+    largeImageURL: '',
+    isLoading: false,
+  };
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.userQuery !== this.state.userQuery) {
+      this.setState(prevState => ({
+        isLoading: true,
+        currentPage: prevState.currentPage + 1,
+      }));
+      this.fetchImages();
+    }
+  }
+  fetchImages = () => {
+    imageFinderAPI
+      .getImages(this.state.userQuery, this.state.currentPage)
+      .then(({ hits }) => {
+        this.setState(prevState => ({
+          images: [...prevState.images, ...hits],
+        }));
+        window.scrollTo({
+          top: document.documentElement.offsetHeight,
+          behavior: 'smooth',
+        });
+      })
+      .catch(error => this.setState({ error: error.message }))
+      .finally(this.setState({ isLoading: false }));
+  };
+  getUserQuery = userInput => {
+    this.setState({
+      userQuery: userInput,
+      images: [],
+      currentPage: 1,
+      error: null,
+    });
+  };
+  onClickImage = largeImageURL => {
+    this.togleModal();
+    this.setState({ largeImageURL: largeImageURL });
+  };
+  togleModal = () => {
+    this.setState(prevState => ({
+      showModal: !prevState.showModal,
+    }));
+  };
+  render() {
+    return (
+      <div className={s.App}>
+        <Searchbar onSubmitHandler={this.getUserQuery} />
+        {this.state.isLoading && <PuffLoader />}
+        {this.state.images.length > 0 && (
+          <ImageGallery>
+            {this.state.images.map(image => (
+              <ImageGalleryItem
+                key={image.id}
+                webformatURL={image.webformatURL}
+                largeImageURL={image.largeImageURL}
+                onCLick={this.onClickImage}
+              />
+            ))}
+          </ImageGallery>
+        )}
+        {this.state.showModal && (
+          <Modal URL={this.state.largeImageURL} togleModal={this.togleModal} />
+        )}
+        {this.state.images.length > 0 && (
+          <Button onClickHandler={this.fetchImages} />
+        )}
+      </div>
+    );
+  }
+}
